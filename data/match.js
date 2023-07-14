18999cbcfe38c9d6baccc66e15613b87c86cc3c3
_ Shreyas (shreyas7@deloitte.com) <shreyas7@your.hostname.com>
2023-07-14 15:31:08 +0530

    match and team data added

---
+++ data/match.js
@@ -0,0 +1,74 @@
+const matches = [
+    {
+      id: "1",
+      date: "2023-07-14",
+      team1: "India",
+      team2: "Australia",
+      result: "India won by 50 runs"
+    },
+    {
+      id: "2",
+      date: "2023-07-15",
+      team1: "England",
+      team2: "New Zealand",
+      result: "New Zealand won by 3 wickets"
+    },
+    {
+      id: "3",
+      date: "2023-07-16",
+      team1: "Pakistan",
+      team2: "West Indies",
+      result: "West Indies won by 2 wickets"
+    },
+    {
+      id: "4",
+      date: "2023-07-17",
+      team1: "Australia",
+      team2: "New Zealand",
+      result: "Australia won by 6 runs"
+    },
+    {
+      id: "5",
+      date: "2023-07-18",
+      team1: "India",
+      team2: "England",
+      result: "India won by 8 wickets"
+    },
+    {
+      id: "6",
+      date: "2023-07-19",
+      team1: "West Indies",
+      team2: "Pakistan",
+      result: "Pakistan won by 4 wickets"
+    },
+    {
+      id: "7",
+      date: "2023-07-20",
+      team1: "Australia",
+      team2: "England",
+      result: "Australia won by 5 wickets"
+    },
+    {
+      id: "8",
+      date: "2023-07-21",
+      team1: "India",
+      team2: "West Indies",
+      result: "India won by 100 runs"
+    },
+    {
+      id: "9",
+      date: "2023-07-22",
+      team1: "New Zealand",
+      team2: "Pakistan",
+      result: "New Zealand won by 2 wickets"
+    },
+    {
+      id: "10",
+      date: "2023-07-23",
+      team1: "England",
+      team2: "Australia",
+      result: "England won by 4 runs"
+    }
+  ];
+
+  module.exports = matches;
---
+++ data/team.js
@@ -0,0 +1,88 @@
+const teams = [
+  {
+    id: "1",
+    name: "India",
+    players: [
+      { id: "1", name: "Virat Kohli", score: 120, wickets: 0 },
+      { id: "2", name: "Rohit Sharma", score: 90, wickets: 1 },
+      { id: "3", name: "Jasprit Bumrah", score: 10, wickets: 4 },
+      { id: "4", name: "KL Rahul", score: 70, wickets: 0 },
+      { id: "5", name: "Ravindra Jadeja", score: 40, wickets: 2 },
+      { id: "6", name: "Mohammed Shami", score: 5, wickets: 3 },
+      { id: "7", name: "Shikhar Dhawan", score: 100, wickets: 0 },
+      { id: "8", name: "Hardik Pandya", score: 60, wickets: 1 },
+      { id: "9", name: "Yuzvendra Chahal", score: 20, wickets: 5 },
+      { id: "10", name: "Bhuvneshwar Kumar", score: 30, wickets: 2 },
+      { id: "11", name: "Ishan Kishan", score: 50, wickets: 0 }
+    ]
+  },
+  {
+    id: "2",
+    name: "Australia",
+    players: [
+      { id: "12", name: "Steve Smith", score: 80, wickets: 0 },
+      { id: "13", name: "David Warner", score: 70, wickets: 0 },
+      { id: "14", name: "Pat Cummins", score: 20, wickets: 3 },
+      { id: "15", name: "Aaron Finch", score: 90, wickets: 0 },
+      { id: "16", name: "Glenn Maxwell", score: 60, wickets: 1 },
+      { id: "17", name: "Mitchell Starc", score: 10, wickets: 4 },
+      { id: "18", name: "Adam Zampa", score: 50, wickets: 2 },
+      { id: "19", name: "Alex Carey", score: 30, wickets: 1 },
+      { id: "20", name: "Marcus Stoinis", score: 40, wickets: 0 },
+      { id: "21", name: "Josh Hazlewood", score: 5, wickets: 3 },
+      { id: "22", name: "Marnus Labuschagne", score: 100, wickets: 0 }
+    ]
+  },
+  {
+    id: "3",
+    name: "England",
+    players: [
+      { id: "23", name: "Joe Root", score: 100, wickets: 1 },
+      { id: "24", name: "Ben Stokes", score: 50, wickets: 2 },
+      { id: "25", name: "Jofra Archer", score: 30, wickets: 4 },
+      { id: "26", name: "Jos Buttler", score: 90, wickets: 0 },
+      { id: "27", name: "Eoin Morgan", score: 70, wickets: 0 },
+      { id: "28", name: "Chris Woakes", score: 20, wickets: 3 },
+      { id: "29", name: "Jonny Bairstow", score: 60, wickets: 1 },
+      { id: "30", name: "Moeen Ali", score: 10, wickets: 2 },
+      { id: "31", name: "Mark Wood", score: 40, wickets: 0 },
+      { id: "32", name: "Adil Rashid", score: 5, wickets: 3 },
+      { id: "33", name: "Sam Curran", score: 50, wickets: 0 }
+    ]
+  },
+  {
+    id: "4",
+    name: "New Zealand",
+    players: [
+      { id: "34", name: "Kane Williamson", score: 110, wickets: 0 },
+      { id: "35", name: "Ross Taylor", score: 60, wickets: 0 },
+      { id: "36", name: "Trent Boult", score: 10, wickets: 3 },
+      { id: "37", name: "Martin Guptill", score: 80, wickets: 0 },
+      { id: "38", name: "Tim Southee", score: 70, wickets: 1 },
+      { id: "39", name: "Lockie Ferguson", score: 20, wickets: 4 },
+      { id: "40", name: "Colin de Grandhomme", score: 30, wickets: 2 },
+      { id: "41", name: "Tom Latham", score: 40, wickets: 0 },
+      { id: "42", name: "Mitchell Santner", score: 5, wickets: 3 },
+      { id: "43", name: "Kyle Jamieson", score: 50, wickets: 0 },
+      { id: "44", name: "Devon Conway", score: 100, wickets: 0 }
+    ]
+  },
+  {
+    id: "5",
+    name: "Pakistan",
+    players: [
+      { id: "45", name: "Babar Azam", score: 120, wickets: 0 },
+      { id: "46", name: "Mohammad Rizwan", score: 90, wickets: 1 },
+      { id: "47", name: "Shaheen Afridi", score: 10, wickets: 4 },
+      { id: "48", name: "Shadab Khan", score: 70, wickets: 0 },
+      { id: "49", name: "Fakhar Zaman", score: 40, wickets: 2 },
+      { id: "50", name: "Hasan Ali", score: 5, wickets: 3 },
+      { id: "51", name: "Sarfaraz Ahmed", score: 100, wickets: 0 },
+      { id: "52", name: "Imad Wasim", score: 60, wickets: 1 },
+      { id: "53", name: "Usman Qadir", score: 20, wickets: 5 },
+      { id: "54", name: "Haris Rauf", score: 30, wickets: 2 },
+      { id: "55", name: "Azam Khan", score: 50, wickets: 0 }
+    ]
+  }
+];
+module.exports = teams;
