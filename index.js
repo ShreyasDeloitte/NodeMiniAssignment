3b10783c6e3c4a2eb6b5d60a9019c725b2410b3f
_ Shreyas (shreyas7@deloitte.com) <shreyas7@your.hostname.com>
2023-07-14 15:27:03 +0530

    mvc and node configured

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("mini assigment");
+const express = require("express");
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello");
+});
+
+const PORT = process.env.PORT | 5001;
+
+app.listen(PORT, ()=>{
+    console.log(`Server running at port ${PORT}`)
+})
